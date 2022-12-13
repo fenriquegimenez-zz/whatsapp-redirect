8d5f4149ae9e7b22dfcf88940ea51a3d17615161
Enrique Gimenez <me@enrique.digital>
2022-12-13 13:54:30 -0300

    bug fixes

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { useRouter } from 'next/router';
 import { Field, ErrorMessage, useFormik, FormikProvider } from 'formik';
 import { FaWhatsapp } from 'react-icons/fa';
 
@@ -11,10 +12,11 @@ interface FormData {
 
 const WaForm = () => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
 
   function redirect(data: FormData) {
     const cleanedPhoneNumber = data.phoneNumber.trim().replaceAll(' ', '');
-    window.location.href = `https://wa.me/+595${cleanedPhoneNumber}?text=${data.text}`;
+    router.push(`https://wa.me/${cleanedPhoneNumber}?text=${data.text}`);
   }
 
   const validatePhoneNumber = (values: FormData) => {
@@ -34,44 +36,46 @@ const WaForm = () => {
   });
 
   return (
-    <FormikProvider value={formik}>
-      <form
-        className="container"
-        style={{
-          maxWidth: '600px',
-        }}
-      >
-        <div className="input-group my-2">
-          <Field
-            ref={inputRef}
-            name="phoneNumber"
-            type="text"
-            className={`form-control ${
-              formik.errors.phoneNumber ? 'is-invalid' : 'is-valid'
-            }`}
-            placeholder="Número de teléfono"
-          />
-        </div>
-        <ErrorMessage name="phoneNumber" />
-        <div className="input-group my-2">
-          <Field
-            name="text"
-            type="textarea"
-            className="form-control"
-            rows={3}
-            placeholder="Introduce el texto que deseas enviar"
-            component="textarea"
-          />
-        </div>
-        <button
-          className="btn btn-large btn-success mt-3"
-          disabled={!formik.dirty}
-          type="submit"
+    <>
+      <FormikProvider value={formik}>
+        <form
+          className="container"
+          style={{
+            maxWidth: '600px',
+          }}
         >
-          Ir al whatsapp <FaWhatsapp />
-        </button>
-      </form>
-    </FormikProvider>
+          <div className="input-group my-2">
+            <Field
+              ref={inputRef}
+              name="phoneNumber"
+              type="text"
+              className={`form-control ${
+                formik.errors.phoneNumber ? 'is-invalid' : 'is-valid'
+              }`}
+              placeholder="Número de teléfono"
+            />
+          </div>
+          <ErrorMessage name="phoneNumber" />
+          <div className="input-group my-2">
+            <Field
+              name="text"
+              type="textarea"
+              className="form-control"
+              rows={3}
+              placeholder="Introduce el texto que deseas enviar"
+              component="textarea"
+            />
+          </div>
+        </form>
+      </FormikProvider>
+      <button
+        className="btn btn-large btn-success mt-3"
+        disabled={!formik.dirty}
+        onClick={() => redirect(formik.values)}
+      >
+        Ir al whatsapp <FaWhatsapp />
+      </button>
+    </>
   );
 };
 
